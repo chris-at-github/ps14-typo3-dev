fc0aa6411191705fa9a7b30d4c60d0a064846de7
Christian Pschorr <pschorr.christian@gmail.com>
2025-06-19 22:39:35 +0200

    Install stylelint

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from "vite";
 import typo3 from "vite-plugin-typo3";
+import checker from 'vite-plugin-checker'
 import path from "path";
 
 export default defineConfig({
-	plugins: [typo3()],
+	plugins: [
+		typo3()
+		// checker({
+		// 	stylelint: {
+		// 		files: ['./src/**/*.{scss,css,sass}']
+		// 	}
+		// })
+	],
 });

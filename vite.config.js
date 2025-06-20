bc0b1cf565c325bb367870123d1128b4b6fef44a
Christian Pschorr <pschorr.christian@gmail.com>
2025-06-20 22:57:39 +0200

    Remove vite-plugin-checker package

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,9 @@
 import { defineConfig } from "vite";
 import typo3 from "vite-plugin-typo3";
-import checker from 'vite-plugin-checker'
 import path from "path";
 
 export default defineConfig({
 	plugins: [
 		typo3()
-		// checker({
-		// 	stylelint: {
-		// 		files: ['./src/**/*.{scss,css,sass}']
-		// 	}
-		// })
 	],
 });
